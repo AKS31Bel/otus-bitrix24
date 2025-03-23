452bb804c70cc56af5cd5d98bfdc5572949c7b20
Bitrix user <bitrix@otus.seo31.site>
2025-03-23 08:12:05 +0000

    lesson-12

---
+++ local/components/otus/exchange.rate/lang/ru/class.php
@@ -0,0 +1,4 @@
+<?php
+
+$MESS['T_EXEPTION_MODULE_NOT_FOUND'] = 'Модуль currency не установлен!';
+$MESS['T_EXEPTION_CURRENCY_FROM_FOUND'] = 'Не указана валюта!';
---
+++ otus/currencies.php
@@ -0,0 +1,22 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+/**
+ * @var CMain $APPLICATION
+ */
+$APPLICATION->IncludeComponent(
+	"otus:exchange.rate", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"CURRENCY_FROM" => "USD",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "86400"
+	),
+	false,
+	array(
+		"HIDE_ICONS" => "N"
+	)
+);
+
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
